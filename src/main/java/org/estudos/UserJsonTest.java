679bfb5ded71509c44027065c9fbad3655cbde0d
José Carlos  Anastácio <jayci@MacBook-Pro-de-Jose.local>
2025-04-02 18:34:51 -0300

    teste validações

--- src/main/java/org/estudos/UserJsonTest.java
+++ src/main/java/org/estudos/UserJsonTest.java
@@ -7,6 +7,8 @@ import io.restassured.response.Response;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
@@ -57,22 +59,92 @@ public class UserJsonTest {
     @Test
     public void deveVerificarLista() {
         given()
+                .when()
+                .get("https://restapi.wcaquino.me/users/3")
+                .then()
+                .statusCode(200)
+                .body("name", containsString("Ana Júlia"))
+                //Verifica a quantidade de objeticos dentro de uma lista (ARRAI) no Json
+
+                .body("filhos", hasSize(2))
+
+                // Valida um valor dentro de uma lista no Jason
+                .body("filhos[0].name", is("Zezinho"))
+                .body("filhos[1].name", is("Luizinho"))
+
+                //Verifica um valor dentro de uma lista
+                .body("filhos.name", hasItem("Luizinho"))
+                .body("filhos.name", hasItems("Luizinho", "Zezinho"));
+    }
+
+    @Test
+    public void deveRetornarErroUsuario() {
+
+        given()
+                .when()
+                .get("https://restapi.wcaquino.me/users/4")
+                .then()
+                .statusCode(404)
+                .body("error", is("Usuário inexistente"));
+
+        }
+
+    @Test
+    public void deveVerificarListaNaRaiz() {
+
+        given()
         .when()
-            .get("https://restapi.wcaquino.me/users/3")
+            .get("https://restapi.wcaquino.me/users")
         .then()
             .statusCode(200)
-            .body("name", containsString("Ana Júlia"))
-             //Verifica a quantidade de objeticos dentro de uma lista (ARRAI) no Json
+                .body("$", hasSize(3))
+        //OR
+            .body("", hasSize(3))
+            .body("$", hasSize(3))
+            .body("name", hasItems("João da Silva","Maria Joaquina","Ana Júlia"))
+            .body("age[1]",is(25))
+        //Representação de uma lista que tenha um objeto
+            .body("filhos.name",hasItem(Arrays.asList("Zezinho","Luizinho")))
 
-            .body("filhos", hasSize(2))
+        //O fno 5678 representa que o valor é Flout
+            .body("salary", contains(1234.5678f, 2500, null));
+    }
 
-            // Valida um valor dentro de uma lista no Jason
-            .body("filhos[0].name", is("Zezinho"))
-            .body("filhos[1].name", is("Luizinho"))
+    @Test
+    public void devoFazerVerificacaoAvancada() {
+        given()
+        .when()
+            .get("https://restapi.wcaquino.me/users")
+        .then()
+            .statusCode(200)
+            .body("$", hasSize(3))
+            .body("age.findAll{it <= 25}.size()", is(2))
+            .body("age.findAll{it <= 25 && it > 20}.size()", is(1))
+            .body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina"))
+            .body("findAll{it.age <= 25}[0].name", is("Maria Joaquina"))
 
-            //Verifica um valor dentro de uma lista
-            .body("filhos.name", hasItem("Luizinho"))
-            .body("filhos.name", hasItems("Luizinho", "Zezinho"))
+            //[-1 inicia a varredura da lista de baixo para cima]
+            .body("findAll{it.age <= 25}[-1].name", is("Ana Júlia"))
+
+            //Verifica a informaçãpo de toda lista
+            .body("find{it.age <= 25}.name", is("Maria Joaquina"))
+
+            .body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia"))
+
+            //Consulta itens com amis de 10 caracteres
+            .body("findAll{it.name.length() > 10}.name", hasItems("João da Silva", "Maria Joaquina"))
+
+            .body("name.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+
+            .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+
+            .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()", allOf(arrayContaining("MARIA JOAQUINA"), arrayWithSize(1)))
+            .body("age.collect{it * 2}", hasItems(60, 50, 40))
+            .body("id.max()", is(3))
+            .body("salary.min()", is(1234.5678f))
+            .body("salary.findAll{it != null}sum()", is(closeTo(3734.5678f,0.001)))
+            .body("salary.findAll{it != null}sum()", allOf(greaterThan(3000d), lessThan(5000d)))
+
 
         ;
 
